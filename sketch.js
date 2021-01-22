df6a66b16050b6129c0fde69c0b50dd9defb9aa2
Srishti1407 <73740987+Srishti1407@users.noreply.github.com>
2021-01-22 23:16:45 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,56 +1,56 @@
 //Create variables here
-var dog,dogimg,happydogimg;
-var foodStock, foodS;
-var database;
-var feed,addFood;
-var fedTime, lastFed;
-var foodObj;
-function preload(){
+var dog,dogimg,happydogimg,database,foods,foodstock
+var database
+function preload()
+{
   //load images here
-  happydogimg = loadImage("dogImg.png");
-  dogimg= loadImage("dogImg1.png");}
+  happydogimg = loadImage("dogImg.png")
+  dogimg = loadImage("dogImg1.png")
+}
+
 function setup() {
-  createCanvas(1000, 600);
-  database = firebase.database();
-  dog = createSprite(700,300);
-  dog.addImage("dog",dogimg);
-  dog.scale =0.5;
-  feed=createButton("Feed the dog"); 
-  feed.position(350,95); 
-  feed.mousePressed(feedDog); 
-  addFood=createButton("Add Food"); 
-  addFood.position(450,95); 
-  addFood.mousePressed(addFoods);   
-  foodStock = database.ref('Food');
-  foodStock.on("value",function(data){
-    foodS = data.val();})
-  foodObj = new Food(foodS,lastFed);}
-function feedDog(){ 
-  dog.addImage("dog",happydogimg); 
-  foodObj.updateFoodStock(foodObj.getFoodStock()-1); 
-  database.ref('/').update({ 
-    Food:foodObj.getFoodStock(),
-    hour:hour()})} 
-function addFoods(){ 
-  foodS++; 
-  database.ref('/').update({ 
-    Food:foodS })} 
+  createCanvas(700, 600);
+  database = firebase.database()
+  dog = createSprite(300,300)
+  dog.addImage("dog",dogimg)
+  dog.scale = 0.5
+  foodstock = database.ref('food')
+  foodstock.on("value",readStock)
+}
+
+function readStock(data){
+  foods = data.val()
+}
+
+//Function to write values in DB
+function writeStock(x){
+  if(x <= 0){
+    x = 0
+  }
+  else{
+    x = x - 1 
+  }
+  database.ref('/').update(
+    {
+      Food:x
+    }
+  )
+}
+
 function draw() {  
   background(46,139,87);
-  fedTime = database.ref('hour');
-  fedTime.on("value",function(data){
-    lastFed = data.val();
-  });
-  fill("black");
-  textSize(30);
-  text("Food Available:" + foodS,200,500);
+
+  //Function to read values from DB
+  if(keyWentDown(UP_ARROW)){
+    writeStock(foods)
+    dog.addImage("dog",happydogimg)
+  } 
+  fill("black")
+  textSize(30)
+  text("Food Available:" + foods,200,500)
   drawSprites();
-  foodObj.display();
-  fill(255,255,254); 
-  textSize(15); 
-  if(lastFed>=12){ 
-    text("Last Feed : "+ lastFed%12 + " PM", 350,30); 
-  }else if(lastFed==0){ 
-    text("Last Feed : 12 AM",350,30); 
-  }else{ 
-    text("Last Feed : "+ lastFed + " AM", 350,30);}}
+  //add styles here
+  fill("black")
+  textSize(30)
+  text("Press the Up arrow to feed the Dog",100,100)
+}
